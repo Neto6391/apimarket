c35636884e837249388d32b99c1f73487c5333f7
Neto6391 <neto6391@gmail.com>
2018-03-15 12:38:06 -0300

    Project Created And REST tested

---
+++ src/main/java/com/neto6391/apimarket/ApimarketApplication.java
@@ -0,0 +1,12 @@
+package com.neto6391.apimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimarketApplication.class, args);
+	}
+}
